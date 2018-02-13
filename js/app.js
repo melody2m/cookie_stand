1039f390a1e951bd363e6f048de3a29897eaf3c7
melody2m <melody2musik@gmail.com>
2018-02-13 14:33:46 -0800

    day 2 constructor, table

--- js/app.js
+++ js/app.js
@@ -1,233 +1,130 @@
 'use strict;'
 
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm','8pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-// first location, 1st and Pike
+var cookStores = []
 
-var firstAndPike = {
-    name: '1st and Pike',
-    minCustHr: 23,
-    maxCustHr: 65,
-    custPerHr: function(){
-        return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
-    },
-    cookiesEachHour: [],
-    cookiesPerCust: 6.3,
-    calcCooksPerHour: function(){
-        for(var i=0; i < hours.length; i++){
-            var round = Math.round(this.custPerHr() * this.cookiesPerCust);
-            this.cookiesEachHour.push(round);
-        };
-    return this.cookiesEachHour;
-    },
+//cookie store constructor
+
+function cookStore(name, minCustHr, maxCustHr, cookiesPerCust, title, loc) {
+    this.name = name;
+    this.minCustHr = minCustHr;
+    this.maxCustHr = maxCustHr;
+    this.cookiesPerCust = cookiesPerCust;
+    this.cookiesEachHour = [];
+    this.totalCooks= 0;
+    this.location= loc;
+    cookStores.push(this);
+};
+
+
+cookStore.prototype.custPerHr = function(){
+    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
+};
+
+cookStore.prototype.calcCooksPerHour = function(){
+    for(var i=0; i < hours.length; i++){
+        this.cookiesEachHour.push(Math.round(this.custPerHr() * this.cookiesPerCust));
+        this.totalCooks += this.cookiesEachHour[i];
+        console.log(this.totalCooks);
+    };
+};
+
+cookStore.prototype.render = function() {
+    this.calcCooksPerHour();
+    var trEl = document.createElement('tr');
     
-    calcTotalCooks: function(){
-        var sumArray = [0];
-        for(var i=0; i < this.cookiesEachHour.length; i++){
-            sumArray.push(sumArray[i] + this.cookiesEachHour[i]);
-            console.log(sumArray[i]);
-        }
-        return sumArray[i];
-    },
+    var tdElFirst = document.createElement('td')
+    tdElFirst.textContent = this.name;
+    trEl.appendChild(tdElFirst);
 
-    render: function() {
-        var ulEl = document.getElementById('loc1');
-        for (var i = 0; i < this.cookiesEachHour.length; i++) {
-            var liEl = document.createElement('li');
-            liEl.textContent = hours[i] + ': ' + this.cookiesEachHour[i];
-        ulEl.appendChild(liEl);
-        }
-        var liTotal = document.createElement('li');
-        liTotal.textContent = 'Total: ' + this.calcTotalCooks();
-        ulEl.appendChild(liTotal);
+    for (var i = 0; i < this.cookiesEachHour.length; i++) {
+        var tdEl = document.createElement('td');
+        tdEl.textContent = this.cookiesEachHour[i];
+        trEl.appendChild(tdEl);
+    }
+    var tdElLast = document.createElement('td')
+    tdElLast.textContent = this.totalCooks;
+    trEl.appendChild(tdElLast);
+
+    cookTable.appendChild(trEl);
+}
+    
+// table head render function
+
+function renderHead(){
+    var trEl = document.createElement('tr');
+    
+    var thElFirst = document.createElement('th')
+    thElFirst.textContent = 'Location';
+    trEl.appendChild(thElFirst);
+    
+    for (var i=0; i < hours.length; i++){
+        var thEl = document.createElement('th');
+        thEl.textContent = hours[i];
+        trEl.appendChild(thEl);
+    }
+
+    var thElLast = document.createElement('th')
+    thElLast.textContent = 'Location Total';
+    trEl.appendChild(thElLast);
+    
+    cookTable.appendChild(trEl);
+}
+
+// create store objects
+
+var firstAndPike = new cookStore('1st and Pike', 23, 65, 6.3);
+var seaTac = new cookStore('SeaTac Airport', 3, 24, 1.2);
+var seattleCenter = new cookStore('Seattle Center', 11, 38, 3.7);
+var capitolHill = new cookStore('Capitol Hill', 20, 38, 2.3);
+var alki = new cookStore('Alki', 2, 16, 4.6);
+
+// render table body function
+
+function renderBody(){
+    for (var i=0; i < cookStores.length; i++){
+        cookStores[i].render();
     }
 }
 
-firstAndPike.custPerHr();
-firstAndPike.calcCooksPerHour();
-firstAndPike.calcTotalCooks();
-firstAndPike.render();
 
-//second location
+function renderFoot(){
+    var hourlySums = [];
+    var hourlyTotal = 0;
+    var grandTotal = 0;
 
-var seaTacAirport = {
-    name: 'SeaTac Airport',
-    minCustHr: 3,
-    maxCustHr: 24,
-    custPerHr: function(){
-        return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
-    },
-    cookiesEachHour: [],
-    cookiesPerCust: 1.2,
-    calcCooksPerHour: function(){
-        for(var i=0; i < hours.length; i++){
-            var round = Math.round(this.custPerHr() * this.cookiesPerCust);
-            this.cookiesEachHour.push(round);
-        };
-    return this.cookiesEachHour;
-    },
-    
-    calcTotalCooks: function(){
-        var sumArray = [0];
-        for(var i=0; i < this.cookiesEachHour.length; i++){
-            sumArray.push(sumArray[i] + this.cookiesEachHour[i]);
-            console.log(sumArray[i]);
+    for (var j=0; j < hours.length; j++){
+        hourlyTotal = 0;
+        for (var i =0; i < cookStores.length; i++){
+            hourlyTotal += cookStores[i].cookiesEachHour[j];
+            grandTotal += cookStores[i].cookiesEachHour[j];
         }
-        return sumArray[i];
-    },
-
-    render: function() {
-        var ulEl = document.getElementById('loc2');
-        for (var i = 0; i < this.cookiesEachHour.length; i++) {
-            var liEl = document.createElement('li');
-            liEl.textContent = hours[i] + ': ' + this.cookiesEachHour[i];
-        ulEl.appendChild(liEl);
-        }
-        var liTotal = document.createElement('li');
-        liTotal.textContent = 'Total: ' + this.calcTotalCooks();
-        ulEl.appendChild(liTotal);
+        hourlySums.push(hourlyTotal);
     }
+    console.log(hourlySums);
+
+    var trEl = document.createElement('tr');
+    
+    var tdElFirst = document.createElement('td')
+    tdElFirst.textContent = 'Totals';
+    trEl.appendChild(tdElFirst);
+    
+    for (var i=0; i < hours.length; i++){
+        var tdEl = document.createElement('td');
+        tdEl.textContent = hourlySums[i];
+        trEl.appendChild(tdEl);
+    }
+
+    var tdElLast = document.createElement('td')
+    tdElLast.textContent = grandTotal;
+    trEl.appendChild(tdElLast);
+    
+    cookTable.appendChild(trEl);
 }
 
-seaTacAirport.custPerHr();
-seaTacAirport.calcCooksPerHour();
-seaTacAirport.calcTotalCooks();
-seaTacAirport.render();
+// call render functions to populate table
 
-//third location
-
-var seattleCenter = {
-    name: 'Seattle Center',
-    minCustHr: 11,
-    maxCustHr: 38,
-    custPerHr: function(){
-        return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
-    },
-    cookiesEachHour: [],
-    cookiesPerCust: 3.7,
-    calcCooksPerHour: function(){
-        for(var i=0; i < hours.length; i++){
-            var round = Math.round(this.custPerHr() * this.cookiesPerCust);
-            this.cookiesEachHour.push(round);
-        };
-    return this.cookiesEachHour;
-    },
-    
-    calcTotalCooks: function(){
-        var sumArray = [0];
-        for(var i=0; i < this.cookiesEachHour.length; i++){
-            sumArray.push(sumArray[i] + this.cookiesEachHour[i]);
-            console.log(sumArray[i]);
-        }
-        return sumArray[i];
-    },
-
-    render: function() {
-        var ulEl = document.getElementById('loc3');
-        for (var i = 0; i < this.cookiesEachHour.length; i++) {
-            var liEl = document.createElement('li');
-            liEl.textContent = hours[i] + ': ' + this.cookiesEachHour[i];
-        ulEl.appendChild(liEl);
-        }
-        var liTotal = document.createElement('li');
-        liTotal.textContent = 'Total: ' + this.calcTotalCooks();
-        ulEl.appendChild(liTotal);
-    }
-}
-
-seattleCenter.custPerHr();
-seattleCenter.calcCooksPerHour();
-seattleCenter.calcTotalCooks();
-seattleCenter.render();
-
-//location 4
-
-var capitolHill = {
-    name: 'Capitol Hill',
-    minCustHr: 20,
-    maxCustHr: 38,
-    custPerHr: function(){
-        return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
-    },
-    cookiesEachHour: [],
-    cookiesPerCust: 2.3,
-    calcCooksPerHour: function(){
-        for(var i=0; i < hours.length; i++){
-            var round = Math.round(this.custPerHr() * this.cookiesPerCust);
-            this.cookiesEachHour.push(round);
-        };
-    return this.cookiesEachHour;
-    },
-    
-    calcTotalCooks: function(){
-        var sumArray = [0];
-        for(var i=0; i < this.cookiesEachHour.length; i++){
-            sumArray.push(sumArray[i] + this.cookiesEachHour[i]);
-            console.log(sumArray[i]);
-        }
-        return sumArray[i];
-    },
-
-    render: function() {
-        var ulEl = document.getElementById('loc4');
-        for (var i = 0; i < this.cookiesEachHour.length; i++) {
-            var liEl = document.createElement('li');
-            liEl.textContent = hours[i] + ': ' + this.cookiesEachHour[i];
-        ulEl.appendChild(liEl);
-        }
-        var liTotal = document.createElement('li');
-        liTotal.textContent = 'Total: ' + this.calcTotalCooks();
-        ulEl.appendChild(liTotal);
-    }
-}
-
-capitolHill.custPerHr();
-capitolHill.calcCooksPerHour();
-capitolHill.calcTotalCooks();
-capitolHill.render();
-
-//location 5
-
-var alki = {
-    name: '1st and Pike',
-    minCustHr: 2,
-    maxCustHr: 16,
-    custPerHr: function(){
-        return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
-    },
-    cookiesEachHour: [],
-    cookiesPerCust: 4.6,
-    calcCooksPerHour: function(){
-        for(var i=0; i < hours.length; i++){
-            var round = Math.round(this.custPerHr() * this.cookiesPerCust);
-            this.cookiesEachHour.push(round);
-        };
-    return this.cookiesEachHour;
-    },
-    
-    calcTotalCooks: function(){
-        var sumArray = [0];
-        for(var i=0; i < this.cookiesEachHour.length; i++){
-            sumArray.push(sumArray[i] + this.cookiesEachHour[i]);
-            console.log(sumArray[i]);
-        }
-        return sumArray[i];
-    },
-
-    render: function() {
-        var ulEl = document.getElementById('loc5');
-        for (var i = 0; i < this.cookiesEachHour.length; i++) {
-            var liEl = document.createElement('li');
-            liEl.textContent = hours[i] + ': ' + this.cookiesEachHour[i];
-        ulEl.appendChild(liEl);
-        }
-        var liTotal = document.createElement('li');
-        liTotal.textContent = 'Total: ' + this.calcTotalCooks();
-        ulEl.appendChild(liTotal);
-    }
-}
-
-alki.custPerHr();
-alki.calcCooksPerHour();
-alki.calcTotalCooks();
-alki.render();
+renderHead();
+renderBody();
+renderFoot();
